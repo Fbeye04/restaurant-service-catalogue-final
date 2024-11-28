1f983f938a414ee9be80b5fca7fe169045021cf1
Fachrezi <107632872+Fbeye04@users.noreply.github.com>
2024-11-28 09:17:10 +0700

    add sharp.js

---
+++ sharp.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+const target = path.resolve(__dirname, 'src/public/images/heros');
+const destination = path.resolve(__dirname, 'src/public/images/heros'); // Output ke src/public
+
+if (!fs.existsSync(destination)) {
+  fs.mkdirSync(destination, { recursive: true });
+}
+
+// Versi desktop dengan kompresi
+sharp(`${target}/hero-image_1.jpg`)
+  .resize(1200)
+  .jpeg({ quality: 80 })
+  .toFile(path.resolve(__dirname, `${destination}/hero-image_1-large.jpg`))
+  .then((info) => {
+    console.log('Large image generated:', info);
+  })
+  .catch((err) => {
+    console.error('Error generating large image:', err);
+  });
+
+// Versi mobile dengan kompresi
+sharp(`${target}/hero-image_1.jpg`)
+  .resize(600)
+  .jpeg({ quality: 80 })
+  .toFile(path.resolve(__dirname, `${destination}/hero-image_1-small.jpg`))
+  .then((info) => {
+    console.log('Small image generated:', info);
+  })
+  .catch((err) => {
+    console.error('Error generating small image:', err);
+  });
