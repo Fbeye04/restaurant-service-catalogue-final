ce09d35fcfe99cb2482ce4576a152fb743b17b72
Fachrezi <107632872+Fbeye04@users.noreply.github.com>
2024-11-28 23:30:53 +0700

    Update sharp.js

--- sharp.js
+++ sharp.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const target = path.resolve(__dirname, 'src/public/images/heros');
-const destination = path.resolve(__dirname, 'src/public/images/heros'); // Output ke src/public
+const destination = path.resolve(__dirname, 'src/public/images/heros');
 
 if (!fs.existsSync(destination)) {
   fs.mkdirSync(destination, { recursive: true });
@@ -13,7 +13,11 @@ if (!fs.existsSync(destination)) {
 // Versi desktop dengan kompresi
 sharp(`${target}/hero-image_1.jpg`)
   .resize(1200)
-  .jpeg({ quality: 80 })
+  .jpeg({
+    quality: 80,
+    progressive: true,
+    force: true,
+  })
   .toFile(path.resolve(__dirname, `${destination}/hero-image_1-large.jpg`))
   .then((info) => {
     console.log('Large image generated:', info);
@@ -25,7 +29,11 @@ sharp(`${target}/hero-image_1.jpg`)
 // Versi mobile dengan kompresi
 sharp(`${target}/hero-image_1.jpg`)
   .resize(600)
-  .jpeg({ quality: 80 })
+  .jpeg({
+    quality: 80,
+    progressive: true,
+    force: true,
+  })
   .toFile(path.resolve(__dirname, `${destination}/hero-image_1-small.jpg`))
   .then((info) => {
     console.log('Small image generated:', info);
